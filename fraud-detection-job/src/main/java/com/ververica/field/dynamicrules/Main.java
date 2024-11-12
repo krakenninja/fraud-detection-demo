dcc4d2176af6efd80076086eedef9cb52bd411b0
krakenninja <cckw.biz@gmail.com>
2024-11-12 19:09:51 +0800

    Modified to use spring boot (version 2.x.x to support Java 8)

--- fraud-detection-job/src/main/java/com/ververica/field/dynamicrules/Main.java
+++ fraud-detection-job/src/main/java/com/ververica/field/dynamicrules/Main.java
@@ -26,13 +26,27 @@ import com.ververica.field.config.Config;
 import com.ververica.field.config.Parameters;
 import org.apache.flink.api.java.utils.ParameterTool;
 
-public class Main {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
 
   public static void main(String[] args) throws Exception {
-    ParameterTool tool = ParameterTool.fromArgs(args);
-    Parameters inputParams = new Parameters(tool);
-    Config config = new Config(inputParams, STRING_PARAMS, INT_PARAMS, BOOL_PARAMS);
-    RulesEvaluator rulesEvaluator = new RulesEvaluator(config);
-    rulesEvaluator.run();
+    SpringApplication.run(
+        Main.class, 
+        args
+    );
+
   }
+
+    @Override
+    public void run(String... args) throws Exception {
+      ParameterTool tool = ParameterTool.fromArgs(args);
+      Parameters inputParams = new Parameters(tool);
+      Config config = new Config(inputParams, STRING_PARAMS, INT_PARAMS, BOOL_PARAMS);
+      RulesEvaluator rulesEvaluator = new RulesEvaluator(config);
+      rulesEvaluator.run();
+    }
 }
